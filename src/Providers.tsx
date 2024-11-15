14c00932d094bf64d8b1032cc423cadb8147c59d
Nicolai Grymer <pronicoxd@gmail.com>
2024-11-15 19:59:46 +0100

    Add SpeedInsights

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import React from "react";
 import { DarkmodeProvider } from "./contexts/DarkmodeProvider";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import { StateProvider } from "./components/survey/state";
 
 type Props = {
